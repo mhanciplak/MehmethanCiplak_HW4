3c4ab5a9095c41dd438e107460690e0f69e76e22
Mehmet Han Çıplak <mehmet.ciplak@boun.edu.tr>
2020-05-04 02:33:08 +0300

    Homework

---
+++ CetBookStore/Controllers/BooksController.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CetBookStore.Data;
+using CetBookStore.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace CetBookStore.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Books
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Books.Include(b => b.Category);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Books/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books
+                .Include(b => b.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // GET: Books/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name");
+            return View();
+        }
+
+        // POST: Books/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,PageCount,Authors,Description,Price,PressYear,StockCount,CategoryId,CreatedDate")] Book book)
+        {
+            book.CreatedDate = DateTime.Now;
+            if (ModelState.IsValid)
+            {
+                _context.Add(book);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", book.CategoryId);
+            return View(book);
+        }
+
+        // GET: Books/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", book.CategoryId);
+            return View(book);
+        }
+
+        // POST: Books/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,PageCount,Authors,Description,Price,PressYear,StockCount,CategoryId,CreatedDate")] Book book)
+        {
+            if (id != book.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(book);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookExists(book.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", book.CategoryId);
+            return View(book);
+        }
+
+        // GET: Books/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books
+                .Include(b => b.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+        
+        // POST: Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        //POST Search
+        
+        public ActionResult Arama(string arama)
+        {
+
+
+            var kategorilist = _context.Categories.ToList();
+            ViewBag.CategoriId = new SelectList(kategorilist, "Id", "Name"); 
+
+
+            var book = from m in _context.Books select m;
+            if (!String.IsNullOrEmpty(arama))
+            {
+                book = book.Where(s => s.Title.Contains(arama));
+            }
+            return View(book.ToList());
+        }
+        [HttpPost]
+        public ActionResult Arama(int categoryId,int buyuk, int kucuk)
+        {
+            var kategorilist = _context.Categories.ToList();
+            ViewBag.CategoriId = new SelectList(kategorilist, "Id", "Name");
+
+            var sorgu = _context.Books.ToList();
+            if(kucuk>0)
+            {
+                
+                sorgu = _context.Books.Where(x => x.CategoryId == categoryId).OrderBy(x => x.Price).ToList();
+            }
+            else if(buyuk>0)
+            {
+                sorgu = _context.Books.Where(x => x.CategoryId == categoryId).OrderBy(x => x.Price).ToList();
+            }
+            else
+            {
+                sorgu = _context.Books.ToList();
+            }
+            return View(sorgu);
+        }
+
+        private bool BookExists(int id)
+        {
+            return _context.Books.Any(e => e.Id == id);
+        }
+    }
+}
